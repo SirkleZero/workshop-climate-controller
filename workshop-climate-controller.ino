9e5062b6aae13ad47cb47d9fd7b419f2072dddd1
Joshua Gall <josh@imtraum.com>
2018-11-21 19:26:44 -0600

    wip

--- workshop-climate-controller.ino
+++ workshop-climate-controller.ino
@@ -27,43 +27,57 @@ Secrets secrets;
 SensorTransmissionResult result;
 IoTUploadResult uploadResult;
 ControllerConfiguration controllerConfiguration;
+bool radioEnabled, internetEnabled, sdCardEnabled, displayEnabled, relayEnabled;
 
 // objects that handle functionality
 HumidityRelayManager relayManager;
-RFM69RXProxy rxProxy;
+RFM69RXProxy radio;
 AdafruitIOProxy httpClient;
 RXTFTFeatherwingProxy display;
 SDCardProxy sdCard;
 
+/*
+we need our modules in the following priority order:
+1. SD Card - Contains all of our configuration. Can't run without this information.
+2. The Display - we can technically function without a display, but we're going to use it to display errors.
+3. Relay Manager - no point in doing any of this if the relay handling isn't there
+4. The RFM69 Radio - Sends us data. Without it, again, no point.
+5. The Internet / adafruit - again, we can function without this.
+*/
 void setup()
 {
 	Serial.begin(115200);
-
 	//while (!Serial); // MAKE SURE TO REMOVE THIS!!!
 
-	sdCard.Initialize();
-	sdCard.LoadSecrets(&secrets);
-	sdCard.LoadConfiguration(&controllerConfiguration);
+	if (sdCard.Initialize().IsSuccessful)
+	{
+		sdCard.LoadSecrets(&secrets);
+		sdCard.LoadConfiguration(&controllerConfiguration);
 
-	httpClient.Initialize(&secrets);
+		if (display.Initialize().IsSuccessful)
+		{
+			display.Clear();
+			display.DrawLayout();
 
-	display.Initialize();
-	display.Clear();
-	display.DrawLayout();
+			display.PrintSensors(SensorData::EmptyData());
+			display.PrintFreeMemory(freeMemory());
 
-	display.PrintSensors(SensorData::EmptyData());
-	display.PrintFreeMemory(freeMemory());
-
-	rxProxy.Initialize();
-
-	relayManager.Initialize(&controllerConfiguration);
+			if (relayManager.Initialize(&controllerConfiguration).IsSuccessful)
+			{
+				if (radio.Initialize().IsSuccessful)
+				{
+					internetEnabled = httpClient.Initialize(&secrets).IsSuccessful;
+				}
+			}
+		}
+	}
 }
 
 void loop()
 {
-	/* 
+	/*
 	Use the emergency shutoff function to shut off the relays if a pre-determined time amount has lapsed. All of this logic is within this method, no other calls are necessary. The KeepAlive() method is essentially a dead man switch that this method uses to either keep things going, or, if the sensor array functionality doesn't transmit anything or we don't receive anything, we shut down power to all our devices.
-	
+
 	This is a safety thing.
 	*/
 	relayManager.EmergencyShutoff();
@@ -71,7 +85,7 @@ void loop()
 	// !!! CRITICAL !!!
 	// the rxProxy listen function needs to execute as often as possible to not miss any messages or acknowledgements. it would be bad to have the loop have a delay call in it, messages will be lost.
 	// DO NOT put a delay call in the loop function!
-	result = rxProxy.Listen();
+	result = radio.Listen();
 
 	if (result.HasResult)
 	{
@@ -80,16 +94,17 @@ void loop()
 
 		relayManager.AdjustClimate(result.Data);
 
-		uploadResult = httpClient.Transmit(result.Data);
-		if (!uploadResult.IsSuccess)
+		if (internetEnabled)
 		{
-			// what should we do if there is an error?
+			uploadResult = httpClient.Transmit(result.Data);
+			if (!uploadResult.IsSuccess)
+			{
+				// what should we do if there is an error?
+			}
 		}
 
 		// calling Initialize on the rxProxy is a total hack. It re-initializes the RF69 radio because the radio head library doesn't handle shared SPI bus very well (apparently). If we don't reinitialize this, the loop will catch only the first transmission, and after that it won't catch anything. This "fixes" that issue. Yes, it's dumb and shared SPI sucks, at least in this case.
-
-		// TODO: I suspect that something in this method periodically fails, causing the application to hang. Look here to add some better error handling.
-		InitializationResult resetResult = rxProxy.Reset();
+		InitializationResult resetResult = radio.Reset();
 		if (!resetResult.IsSuccessful)
 		{
 			// something didn't work here, so let's display an error message!
